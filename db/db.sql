2db60e683c74a8fc758245ab928a86bb8a326bcb
adjanaa <andnaneadjana@yahoo.fr>
2017-04-21 12:51:32 -0400

    Update db.sql

--- db/db.sql
+++ db/db.sql
@@ -7,7 +7,24 @@ create table article (
   paragraphe varchar(500)
 );
 
-insert into article values (1,'journal','ASSFDGFGGHMH','SADASFDGHM',"2009-10-17",'trop de cul');
-insert into article values (2,'victoire','ligue des champions','KK_MM',"2017-03-11",'barca a gagne son match');
-insert into article values (3,'gloire','web de 2','FF_MM',"2017-04-11",'bon');
-insert into article values (4,'televise','journal','SADASFDGHM',"2004-10-17",'Jaime les codes');
+create table users (
+  id integer primary key,
+  utilisateur varchar(25),
+  email varchar(100),
+  salt varchar(32),
+  hash varchar(128)
+);
+
+create table sessions (
+  id integer primary key,
+  id_session varchar(32),
+  utilisateur varchar(25)
+);
+
+create table compte (
+  id integer primary key,
+  mail varchar(32),
+  jeton varchar(32),
+  dateEnvoie text
+);
+
